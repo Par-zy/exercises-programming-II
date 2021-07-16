c0022bcaa24e199e88026063d11680356a207416
Par-zy <j.davi4242@gmail.com>
2021-07-16 00:18:16 -0300

    add question3 code

--- Prog II - Activity 1/Questão3/Maior.java
+++ Prog II - Activity 1/Questão3/Maior.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 public class Maior {
     public static void main(String args[]) {
-    
+        int a, b;
+        Scanner read = new Scanner(System.in);
+
+        System.out.println("Insira o primeiro número: ");
+        a = read.nextInt();
+        System.out.println("Insira o segundo número: ");
+        b = read.nextInt();
+
+        if (a>b) {
+            System.out.println("O número " + a + " é maior que " + b);
+        } else {
+            System.out.println("O número " + b + " é maior que " + a);
+            
+        }
     }  
 }
